98d882175a77afb50ba46cb7269ab723b3f39b28
WXZA <3130001461@qq.com>
2021-12-22 15:14:14 +0800

    init

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.browserAction.onClicked.addListener(function () {
+    // 当点击扩展图标时，执行...
+    chrome.sessions.getRecentlyClosed(
+        chrome.tabs.Tab, function (sessions) {
+            console.log(sessions);
+            chrome.sessions.restore(sessions[0].tab.sessionId, function(session){})
+        });
+});
